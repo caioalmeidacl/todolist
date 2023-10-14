99883f7e703aa6ae8da950ed4afd08a6c969dc78
CAIO ALMEIDA <caioalmeidaacl@gmail.com>
2023-10-14 12:13:17 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/caiocarvalho/todolist/task/TaskController.java
@@ -0,0 +1,64 @@
+package br.com.caiocarvalho.todolist.task;
+
+import br.com.caiocarvalho.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser =  request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        // Validation
+        var currentDate = LocalDateTime.now();
+        // StartAt || endAt > CurrentDate
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de inicio / data de termino deve ser maior do que a data atual");
+        }
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de inicio deve ser menor do que a data de termino");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser =  request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+    // http:localhost:8080/tasks/id-da-task-pra-alterar
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel,@PathVariable UUID id, HttpServletRequest request) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa nao encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario nao tem permissao para alterar essa tarefa");
+        }
+        Utils.copyNonNullProperties(taskModel, task);
+        var  taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
+    }
+}
